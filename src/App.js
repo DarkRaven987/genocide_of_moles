f5e05fde05e7c095d5e61b99ff3e161e94c4bb06
darkraven987 <darkraven987@gmail.com>
2019-04-08 10:57:20 +0300

    See moles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import mole_null from './img/mole_null.png'
+import mole_true from './img/mole_true.png'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Ready to work
+      <div className="container">
+        <div className="App-header">
+          GENOCIDE OF MOLES
+        </div>
+        <div className="Game-Field">
+          <div className="onPlay-zone">
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+            <div className="onClick-element">
+              <img src={mole_true} alt="QWERTY"/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
