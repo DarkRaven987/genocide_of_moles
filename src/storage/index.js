251ad41a2e42ee4b249d0dfe5ea85766213d4489
darkraven987 <darkraven987@gmail.com>
2019-04-08 12:02:10 +0300

    Created full redux store

--- src/storage/index.js
+++ src/storage/index.js
@@ -6,7 +6,11 @@ const storage = {
         {id: 4, isMole: false},
         {id: 5, isMole: false},
         {id: 6, isMole: false},
-        ]
+        ],
+    gameDifficult: 1,
+    score: { current: 0, max: 100},
+    failTimes: 0,
+    currentTime: 0
 };
 
 const reducer = (store=storage, action) => {
